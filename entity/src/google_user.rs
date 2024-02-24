80d5ee65787821b78a125729f0f940caccb96c61
Brandon Li <sirbrandonthenerd@gmail.com>
2024-02-24 11:33:54 -0500

    relation on google_user

--- entity/src/google_user.rs
+++ entity/src/google_user.rs
@@ -12,6 +12,19 @@ pub struct Model {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::user::Entity",
+        from = "Column::UserId",
+        to = "super::user::Column::UserId"
+    )]
+    User,
+}
+
+impl Related<super::user::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::User.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}
