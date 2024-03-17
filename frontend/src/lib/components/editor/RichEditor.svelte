1b8e266d71391bd869b2166d8506ec437c49e650
Brandon Li <sirbrandonthenerd@gmail.com>
2024-03-17 11:24:20 -0400

    switch to using tiptap

--- frontend/src/lib/components/editor/RichEditor.svelte
+++ frontend/src/lib/components/editor/RichEditor.svelte
@@ -2,89 +2,63 @@
 	import Button from '$src/lib/components/ui/button/button.svelte';
 	import { Bold, Italic, type Icon } from 'lucide-svelte';
 
-	import { EditorState, type Command, Plugin } from 'prosemirror-state';
-	import { EditorView } from 'prosemirror-view';
-	import { Schema, DOMParser, MarkType, Node } from 'prosemirror-model';
-	import { schema } from 'prosemirror-schema-basic';
-	import { addListNodes } from 'prosemirror-schema-list';
-	import { exampleSetup } from 'prosemirror-example-setup';
-	import { toggleMark } from 'prosemirror-commands';
+	import { Editor } from '@tiptap/core';
+	import StarterKit from '@tiptap/starter-kit';
 
-	import { onMount } from 'svelte';
-	import 'prosemirror-menu/style/menu.css';
+	import { onMount, onDestroy } from 'svelte';
 
-	// Mix the nodes from prosemirror-schema-list into the basic schema to
-	// create a schema with list support.
-	const mySchema = new Schema({
-		nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block'),
-		marks: schema.spec.marks
-	});
-
+	let editor: Editor;
 	let element: HTMLElement;
-	let view: EditorView;
-
-	class Updater {
-		update() {
-			const new_active = new Array(menu_items.length);
-			for (let i = 0; i < active.length; i++) {
-				new_active[i] = menu_items[i].active();
-			}
-			active = new_active;
-		}
-	}
-	const updater = new Plugin({
-		view(_) {
-			return new Updater();
-		}
-	});
 
 	onMount(() => {
-		view = new EditorView(element, {
-			state: EditorState.create({
-				doc: DOMParser.fromSchema(mySchema).parse(element),
-				plugins: [...exampleSetup({ schema: mySchema, menuBar: false }), updater]
-			})
+		editor = new Editor({
+			element,
+			extensions: [StarterKit],
+
+			onTransaction: () => {
+				editor = editor;
+				const new_active = new Array(menu_items.length);
+				for (let i = 0; i < active.length; i++) {
+					new_active[i] = menu_items[i].active();
+				}
+				active = new_active;
+			}
 		});
 	});
 
+	type ChainedCommands = ReturnType<typeof editor.chain>;
+	type Command = (c: ChainedCommands) => ChainedCommands;
 	type MenuItem = {
 		title: string;
-		command: Command;
 		icon: typeof Bold;
+		command: Command;
 		active: () => boolean;
 	};
 	type Dropdown = [];
 
-	function markActive(type: MarkType) {
-		const s = view.state.selection;
-		if (s.empty) return !!type.isInSet(view.state.storedMarks || s.$from.marks());
-		else return view.state.doc.rangeHasMark(s.from, s.to, type);
-	}
-
 	const menu_items: MenuItem[] = [
 		{
 			title: 'Toggle Bold',
-			command: toggleMark(schema.marks.strong),
 			icon: Bold,
-			active: () => markActive(schema.marks.strong)
+			command: (c) => c.toggleBold(),
+			active: () => editor.isActive('bold'),
 		},
 		{
 			title: 'Toggle Italic',
-			command: toggleMark(schema.marks.em),
 			icon: Italic,
-			active: () => markActive(schema.marks.em)
+			command: (c) => c.toggleItalic(),
+			active: () => editor.isActive('italic'),
 		}
 	];
 	let active = new Array(menu_items.length);
 
-	const execute = (cmd: Command) => {
-		view.focus();
-		cmd(view.state, view.dispatch, view);
+	const execute = (command: Command) => {
+		command(editor.chain().focus()).run();
 	};
 </script>
 
-<div class="prosemirror-editor flex h-full w-full flex-col">
-	<div class="flex gap-0.5 p-0.5">
+<div class="editor flex h-full w-full flex-col" bind:this={element}>
+	<div class="flex gap-0.5 p-0.5 border-b">
 		{#each menu_items || active as { title, command, icon }, i}
 			<Button
 				variant={active[i] ? 'default' : 'outline'}
@@ -96,11 +70,6 @@
 			</Button>
 		{/each}
 	</div>
-	<div
-		class="prosemirror-wrapper border-input flex h-full w-full flex-col
-		overflow-auto border-t"
-		bind:this={element}
-	/>
 </div>
 
 <style lang="postcss">
