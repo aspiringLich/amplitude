d7151a501757b66ec33c064447a039fa1b6c4f46
Brandon Li <sirbrandonthenerd@gmail.com>
2024-03-17 18:10:22 -0400

    sanitize html

--- frontend/src/routes/edit/schema.ts
+++ frontend/src/routes/edit/schema.ts
@@ -1,15 +1,15 @@
 import type { Infer, SuperValidated } from 'sveltekit-superforms';
-import { z } from 'zod';
+import { z, type RefinementCtx } from 'zod';
 
+export const sanitize_html = async (arg: string, _: RefinementCtx) => {
+	const sanitizeHtml = await import('sanitize-html');
+	return sanitizeHtml.default(arg);
+}
 export const exerciseSchema = z.object({
-	title: z
-		.string()
-		.trim()
-		.min(5)
-		.max(32),
-	description: z.string().max(6000),
+	title: z.string().trim().min(5).max(32),
+	description: z.string().max(6000).superRefine(sanitize_html),
 	generator: z.string().max(6000),
-	starting_code: z.string().max(6000),
+	starting_code: z.string().max(6000)
 });
 
 export type ExerciseScheme = typeof exerciseSchema;
