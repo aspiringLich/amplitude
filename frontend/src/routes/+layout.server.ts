a10b1e9db004c9f19318a39d0a7aedf55a4917c8
Brandon Li <sirbrandonthenerd@gmail.com>
2024-03-17 17:55:54 -0400

    fix sessions

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,11 +1,11 @@
 export type AvatarData = { name: string; avatar_url: string };
 
 export const load = async ({ fetch, cookies }) => {
-	const res = await fetch('/api/auth/session', { method: 'GET' });
-	if (res.ok) {
-		const data: AvatarData = await res.json();
-		return { avatar: data };
-	} else {
-		cookies.delete('session', { path: '/'});
+	if (cookies.get('session')) {
+		const res = await fetch('/api/auth/session', { method: 'GET' });
+		if (res.ok) {
+			const data: AvatarData = await res.json();
+			return { avatar: data };
+		}
 	}
 };
